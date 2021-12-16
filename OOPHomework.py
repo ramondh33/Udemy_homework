78fbbacc01541724113bc3d4a0dbf9dcf978e889
ramondh33 <ramon.d.hernandez28@gmail.com>
2021-12-15 23:58:45 -0600

    Finished Homework

--- OOPHomework.py
+++ OOPHomework.py
@@ -1,12 +1,12 @@
 # Importing cmath due to bug in math not letting me use .sqrt
-from cmath import sqrt
+from math import sqrt
 
 # Creating class to create custom methods for distance and slope
 class Line():
     
-    def __init__(self,coor1,coor2):
-        self.coor1 = coor1
-        self.coor2 = coor2
+    def __init__(self,coordinate1,coordinate2):
+        self.coordinate1 = coordinate1
+        self.coordinate2 = coordinate2
         
     # Creating distance method and how it should behave
     def distance(self):
@@ -16,25 +16,26 @@ class Line():
         
         stepTwo = (coordinate1[1] - coordinate2[1])
         
-        stepThree = ((stepOne) * 2) + ((stepTwo) * 2)
+        stepThree = ((stepOne) ** 2) + ((stepTwo) ** 2)
         
         stepFour = sqrt(stepThree)
         
         print(stepFour)
         
         # Math formula to figure out what the distance is
-        print(sqrt((coordinate1[0] - coordinate2[0]) * 2 + (coordinate1[1] - coordinate2[1] * 2)))
+       # print(sqrt((coordinate1[0] - coordinate2[0]) ** 2 + (coordinate1[1] - coordinate2[1] ** 2)))
         
        
     # Math formula for slopes 
     def slope(self):
         
         # Having same problems as above with distance 
-        print((coordinate2[1] - coordinate2[0]) / (coordinate1[1] - coordinate1[0]))
+        print((coordinate2[1] - coordinate1[1]) / (coordinate2[0] - coordinate1[0]))
+        
 
 # Variable assignment 
-coordinate1 = (2,4)
-coordinate2 = (1,7)
+coordinate1 = (3,2)
+coordinate2 = (8,10)
 
 # Assigning class to test variable 
 test = Line(coordinate1,coordinate2)
