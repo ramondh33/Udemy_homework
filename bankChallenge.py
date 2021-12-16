2d6966e0ede9c659ccadff816556a43bd5d3895c
ramondh33 <ramon.d.hernandez28@gmail.com>
2021-12-16 01:51:31 -0600

    Cleaned the code using Pylint

--- bankChallenge.py
+++ bankChallenge.py
@@ -1,33 +1,33 @@
 # Create a class with two attributes
 class Account():
-    
-    # Creating Init to associate parameters
+
+# Creating Init to associate parameters
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
-    
-    # Creating functions to see balance
+
+# Creating functions to see balance
     def accBalance(self):
         print(f"Your balance is {self.balance}")
-        
-    # Creating functions to update balance based on deposit amount    
+
+# Creating functions to update balance based on deposit amount    
     def deposit(self,depAmount):
         self.balance = self.balance + depAmount
 
         print(f"Your new balance is {self.balance}.")
-    
-    # Creating functions to update balance based on withdrawal amount    
+
+# Creating functions to update balance based on withdrawal amount    
     def withdrawal(self,withdraw):
-        
-        # If statement checking if funds are available to withdrawal
+
+# If statement checking if funds are available to withdrawal
         if withdraw > self.balance:
             print("Insufficient Funds!")
-         
-         # Updating balance amount 
+
+# Updating balance amount 
         else:  
             self.balance = self.balance - withdraw
             print(f"Your new balance is {self.balance}")
-        
+
 accountOne = Account("Jose", 100)
 
 print(f"Account Name: {accountOne.name}\nAccount Balance: ${accountOne.balance}")
